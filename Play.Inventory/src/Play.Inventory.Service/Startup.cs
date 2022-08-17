953eacc24f1193588653e05230754e49b1221b55
Leonel Rivas <leonrivas04@gmail.com>
2022-08-17 09:53:15 -0600

    add random time

--- Play.Inventory/src/Play.Inventory.Service/Startup.cs
+++ Play.Inventory/src/Play.Inventory.Service/Startup.cs
@@ -30,13 +30,15 @@ namespace Play.Inventory.Service
             services.AddMongo()
                     .AddMongoRepository<InventoryItem>("inventoryitems");
 
+            Random jitterer = new Random();
+
             services.AddHttpClient<CatalogClient>(client =>
             {
                 client.BaseAddress = new Uri("https://localhost:5001");
             })
             .AddTransientHttpErrorPolicy(builder => builder.Or<TimeoutRejectedException>().WaitAndRetryAsync(
                 5,
-                retryAttemps => TimeSpan.FromSeconds(Math.Pow(2, retryAttemps)),
+                retryAttemps => TimeSpan.FromSeconds(Math.Pow(2, retryAttemps)) + TimeSpan.FromMilliseconds(jitterer.Next(0, 1000)),
                 onRetry: (outcome, timespan, retryAttemp) =>
                 {
                     var serviceProvider = services.BuildServiceProvider();
